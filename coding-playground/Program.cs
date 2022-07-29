4e73e3bfd280db11c7627902afb990194cae0604
ShervilG <wintergpt45@gmail.com>
2022-07-29 23:14:26 +0530

    Add project files.

---
+++ coding-playground/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Func<int, int> func = (x) => x * x;
+
+            var list = new List<int>();
+            list.Add(1);
+            list.Add(12);
+
+            list = list.FindAll((node) => node % 2 == 0); 
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(fact(item));
+            }
+        }
+
+        static int fact(int number)
+        {
+            if (number == 1)
+                return 1;
+            return fact(number - 1) * number;
+        }
+    }
+}
